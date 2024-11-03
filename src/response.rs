ca1c03926ff766ed005ebd9ebac10924a0cc3876
shirayuki <shirayuki@46ki75.com>
2024-11-03 09:22:31 +0900

    feat: Update Response struct to support optional holiday names and add conversion from NaiveDate

--- src/response.rs
+++ src/response.rs
@@ -3,7 +3,7 @@ use serde::Serialize;
 
 #[derive(Debug, Serialize)]
 pub struct Response {
-    pub name: String,
+    pub name: Option<String>,
     pub date: String,
     pub year: i32,
     pub month: u32,
@@ -15,13 +15,27 @@ pub struct Response {
 impl From<crate::holiday::Holiday> for Response {
     fn from(holiday: crate::holiday::Holiday) -> Self {
         Self {
-            name: holiday.name,
+            name: Some(holiday.name),
             date: holiday.date.to_string(),
             year: holiday.date.year(),
             month: holiday.date.month(),
             day: holiday.date.day(),
             public: true,
             holiday: true,
+        }
+    }
+}
+
+impl From<chrono::NaiveDate> for Response {
+    fn from(date: chrono::NaiveDate) -> Self {
+        Self {
+            name: None,
+            date: date.to_string(),
+            year: date.year(),
+            month: date.month(),
+            day: date.day(),
+            public: false,
+            holiday: false,
         }
     }
 }
