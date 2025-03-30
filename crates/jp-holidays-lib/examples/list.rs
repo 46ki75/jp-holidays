655bd7b5684a11991e0012dbb301f2aebd863661
Chomolungma Shirayuki <shirayuki@46ki75.com>
2025-03-30 12:26:36 +0000

    docs: Update example to include comments and improve output formatting

--- crates/jp-holidays-lib/examples/list.rs
+++ crates/jp-holidays-lib/examples/list.rs
@@ -4,11 +4,12 @@ use jp_holidays_lib::client::Client;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let client = Client::init().await?;
 
+    // 公開されている祝日をすべて取得します
     let holidays = client.list_holidays();
 
-    for holiday in holidays.iter().take(10) {
-        let (date, name) = holiday;
-        println!("{}, {}", date, name);
+    // 今回の例では10日分だけ出力
+    for (date, name) in holidays.iter().take(10) {
+        println!("{} | {}", date, name);
     }
 
     Ok(())
