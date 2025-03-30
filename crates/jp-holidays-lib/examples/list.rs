b2c0298cf329e5717ed7b0313d3b85e13ba81712
Chomolungma Shirayuki <shirayuki@46ki75.com>
2025-03-30 12:31:22 +0000

    feat: Update example to fetch holidays for a specific year using NaiveDate

--- crates/jp-holidays-lib/examples/list.rs
+++ crates/jp-holidays-lib/examples/list.rs
@@ -1,14 +1,21 @@
+use chrono::NaiveDate;
 use jp_holidays_lib::client::Client;
+use std::ops::Bound::{Excluded, Included};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let client = Client::init().await?;
 
-    // 公開されている祝日をすべて取得します
-    let holidays = client.list_holidays();
+    // 2018年 の祝日のみを取得します。
+    let start = NaiveDate::from_ymd_opt(2018, 1, 1).ok_or("存在しない日付です".to_string())?;
+    let end = NaiveDate::from_ymd_opt(2019, 1, 1).ok_or("存在しない日付です".to_string())?;
 
-    // 今回の例では10日分だけ出力
-    for (date, name) in holidays.iter().take(10) {
+    // 公開されている祝日をすべて取得します。その後範囲を絞ります。
+    let holidays_2018 = client
+        .list_holidays()
+        .range((Included(start), Excluded(end)));
+
+    for (date, name) in holidays_2018 {
         println!("{} | {}", date, name);
     }
 
